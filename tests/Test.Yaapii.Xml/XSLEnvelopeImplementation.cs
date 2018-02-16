7bddb82a8a62388d49927c71c1888a1db436425b
Christian Sauer <postancs@gmx.de>
2018-02-16 18:22:25 +0100

    refactor: rename

---
+++ tests/Test.Yaapii.Xml/XSLEnvelopeImplementation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using Yaapii.Atoms;
+using Yaapii.Xml;
+
+namespace Yaapii.Xml
+{
+    public sealed class XSLEnvelopeImplementation : XSLEnvelope
+    {
+        public XSLEnvelopeImplementation(IText src, XmlResolver sources, IDictionary<string, object> map) : base(
+            src,
+            sources,
+            map
+        )
+        { }
+    }
+}
