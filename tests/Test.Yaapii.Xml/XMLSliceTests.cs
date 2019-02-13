6d8c856f21610da433a7d2e73a7b7fb0e204fd76
Miro Tunk <miro.tunk@icarus-consult.de>
2019-02-13 17:38:26 +0100

    feat: add slicing IXML

---
+++ tests/Test.Yaapii.Xml/XMLSliceTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Yaapii.Xml.Test
+{
+    public sealed class XMLSliceTests
+    {
+        [Fact]
+        public void CreatesNewDocument()
+        {
+            var node = new XMLSlice("<root><slice>text</slice></root>").Nodes("/root/slice")[0].AsNode();
+            Assert.Equal(
+                node.ToString(),
+                node.Document.ToString()
+            );
+        }
+    }
+}
