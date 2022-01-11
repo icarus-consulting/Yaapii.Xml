1d1f137d83df31177ab5c18db2b867eae03a3f32
Christian Sauer <postancs@gmx.de>
2022-01-11 15:45:22 +0100

    docs: update copyright date

--- src/Yaapii.Xml/XMLNumber.cs
+++ src/Yaapii.Xml/XMLNumber.cs
@@ -1,6 +1,6 @@
 ﻿// MIT License
 //
-// Copyright(c) 2019 ICARUS Consulting GmbH
+// Copyright(c) 2022 ICARUS Consulting GmbH
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -49,7 +49,7 @@ namespace Yaapii.Xml
             this.number =
                 new ScalarOf<INumber>(() =>
                     new NumberOf(
-                        new XMLString(xml, xpath).Value(), 
+                        new XMLString(xml, xpath).Value(),
                         provider
                     )
                 );
